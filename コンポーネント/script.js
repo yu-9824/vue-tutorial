a0354c0c910fa8eaa96da642854c9b5d6fd58445
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-05-30 22:13:54 +0900

    global v.s. local component.

--- コンポーネント/script.js
+++ コンポーネント/script.js
@@ -33,3 +33,41 @@ var app2 = new Vue({
     }
 })
 
+
+// ================================
+// appとapp2のやり方をlocal定義にしただけ．しかし，これをすると結果は正しく表示されるもののエラーコードが出る．
+// ================================
+
+component3 = {
+    template: `
+    <div id='app-4'>
+        <comp-4/>
+    </div>
+    `
+}
+
+var app3 = new Vue ({
+    el: '#app-3',
+    components: {
+        'comp-3': component3,
+    },
+})
+
+component4 = {
+    data: function() {
+        return {
+            message: 'abc',
+        }
+    },
+    template: '<p> {{message}} </p>',
+}
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        message: '',
+    },
+    components: {
+        'comp-4': component4
+    }
+})
