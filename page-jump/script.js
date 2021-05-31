94fe88dbec08c24fbbd860356d8850056acf908c
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-05-31 16:55:18 +0900

    I've done some research on vue-router, but all I've found is that it's not necessary for me at the moment.

--- page-jump/script.js
+++ page-jump/script.js
@@ -1,67 +1,41 @@
+// 1. ルートコンポーネントを定義します
+// 他のファイルからインポートすることもできます
+const Index = { template: '<div>index</div>' }
+const Work = { template: '<div>work</div>' }
+
+
+
+// 2. ルートをいくつか定義します
+// 各ルートは 1 つのコンポーネントとマッピングされる必要があります。
+// このコンポーネントは実際の `Vue.extend()`、
+// またはコンポーネントオプションのオブジェクトでも構いません。
+// ネストされたルートに関しては後で説明します
+const routes = [
+    {
+      path: '/',           //パスの設定
+      component: Index,    //表示するコンポーネント
+      name: 'index',       //ルートの名前を指定
+    },
+    {
+      path: '/work',            
+      component: Work,
+      name: 'work',
+    },
+  ]
+
+// 3. ルーターインスタンスを作成して、ルートオプションを渡します
+// 追加のオプションをここで指定できますが、
+// この例ではシンプルにしましょう
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes // `routes: routes` の短縮表記
+})
+
+// 4. root となるインスタンスを作成してマウントします
+// アプリケーション全体がルーターを認知できるように、
+// ルーターをインジェクトすることを忘れないでください。
 var app = new Vue({
-el: '#app',
-data: {
-    message: 'Hello World!'
-}
-})
-
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-})
-
-var app3 = new Vue({
-    el: '#app-3',
-    data: {
-        seen: false
-    }
-})
-
-var app4 = new Vue({
-    el: '#app-4',
-    data: {
-        todos: [
-            {text: 'Learn JavaScript'},
-            {text: 'Learn Vue'},
-            {text: 'Build something awesome'}
-        ]
-    }
-})
-
-var app5 = new Vue({
-    el: '#app-5',
-    data: {
-        message: 'Hello Vue.js'
-    },
-    methods: {
-        reverseMessage: function() {
-            this.message = this.message.split('').reverse().join('')
-        }
-    },
-})
-
-var app6 = new Vue({
-    el: '#app-6',
-    data: {
-        message: 'Hello Vue!'
-    }
-})
-
-
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>',
-})
-
-var app7 = new Vue({
-    el: '#app-7',
-    data: {
-        groceryList: [
-            {id: 0, text: 'Vegetables'},
-            {id: 1, text: 'Cheese'},
-            {id: 2, text: 'Whatever else humans are supposed to eat'}
-        ]
-    }
-})
+    router,
+  }).$mount('#app')
+  
+  // これで開始です!
